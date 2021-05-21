656c11e6d1c480bf56c2007be20e7ef2faaa7307
Vaibhavbairagi <vaibhavbairagi105@gmail.com>
2021-05-21 23:26:43 +0530

    Added send message functionality and load previous messages

--- app/src/main/java/com/vaibhav/letschat/OneToOneConversationActivity.java
+++ app/src/main/java/com/vaibhav/letschat/OneToOneConversationActivity.java
@@ -1,28 +1,37 @@
 package com.vaibhav.letschat;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.twilio.conversations.CallbackListener;
 import com.twilio.conversations.Conversation;
+import com.twilio.conversations.ConversationListener;
 import com.twilio.conversations.ErrorInfo;
 import com.twilio.conversations.Message;
+import com.twilio.conversations.Participant;
 import com.vaibhav.letschat.adapters.ConversationsMessageRVAdapter;
+
+import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class OneToOneConversationActivity extends AppCompatActivity {
+public class OneToOneConversationActivity extends AppCompatActivity implements ConversationListener {
 
     private static final String TAG = "OneToOneConversation";
     ArrayList<Message> messages = new ArrayList<>();
@@ -33,6 +42,13 @@ public class OneToOneConversationActivity extends AppCompatActivity {
     Conversation conversation;
     Context context;
     ConversationsMessageRVAdapter conversationsMessageRVAdapter;
+    EditText sendMsgTextET;
+    ImageButton sendIBtn, attachIBtn;
+    ProgressBar topProgress;
+
+    boolean isOldMessagesLoading = false;
+
+    private final int LOAD_MESSAGE_COUNT = 30;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,6 +62,10 @@ public class OneToOneConversationActivity extends AppCompatActivity {
         convoNameTv = findViewById(R.id.iv_otoc_convoName);
         convoImgIv = findViewById(R.id.iv_otoc_profile_image);
         msgRecyclerView = findViewById(R.id.messageRecyclerView);
+        sendIBtn = findViewById(R.id.ib_otoc_send_msg);
+        attachIBtn = findViewById(R.id.ib_otoc_attach);
+        sendMsgTextET = findViewById(R.id.et_otoc_send_msg_text);
+        topProgress = findViewById(R.id.progress_top_otoc);
 
         conversationsMessageRVAdapter = new ConversationsMessageRVAdapter(messages, context);
         LinearLayoutManager layoutManager = new LinearLayoutManager(this);
@@ -62,7 +82,7 @@ public class OneToOneConversationActivity extends AppCompatActivity {
 
             @Override
             public void onError(ErrorInfo errorInfo) {
-                Log.d(TAG, "onError: "+errorInfo);
+                Log.d(TAG, "onError: " + errorInfo);
                 Toast.makeText(context, "Failed to start conversation, try later", Toast.LENGTH_SHORT).show();
             }
         });
@@ -77,13 +97,156 @@ public class OneToOneConversationActivity extends AppCompatActivity {
         });
         //todo: change this to display the other user name
         convoNameTv.setText(conversation.getFriendlyName());
-        //gets last 30 messages
-        conversation.getLastMessages(30, new CallbackListener<List<Message>>() {
+        conversation.addListener(this);
+        loadLastMessages();
+        sendIBtn.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(List<Message> result) {
-                messages.addAll(result);
-                conversationsMessageRVAdapter.notifyDataSetChanged();
+            public void onClick(View view) { ;
+                String msg = sendMsgTextET.getText().toString();
+                if (!msg.isEmpty()) {
+                    sendMsgTextET.setText("");
+                    sendMessage(msg);
+                } else {
+                    Toast.makeText(context, "Enter a msg to send", Toast.LENGTH_SHORT).show();
+                }
             }
         });
+    }
+
+    private void sendMessage(String messageBody) {
+        if (conversation != null) {
+            Message.Options options = Message.options().withBody(messageBody);
+            Log.d(TAG, "Message created");
+            conversation.sendMessage(options, new CallbackListener<Message>() {
+                @Override
+                public void onSuccess(Message message) {
+                    Log.d(TAG, "Message sent");
+                    //todo: implement ticks
+                }
+            });
+        }
+    }
+
+    private void loadLastMessages() {
+        topProgress.setVisibility(View.VISIBLE);
+        //gets last 30 messages
+        conversation.getLastMessages(LOAD_MESSAGE_COUNT, new CallbackListener<List<Message>>() {
+            @Override
+            public void onSuccess(List<Message> result) {
+                //todo; add all message to start
+                if (result.size() > 0) {
+                    topProgress.setVisibility(View.GONE);
+                    messages.addAll(result);
+                    conversationsMessageRVAdapter.notifyDataSetChanged();
+                    initScrollUpToLoadMoreFeature();
+                }
+            }
+        });
+    }
+
+    private void initScrollUpToLoadMoreFeature() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            msgRecyclerView.setOnScrollChangeListener(new View.OnScrollChangeListener() {
+                @Override
+                public void onScrollChange(View view, int i, int i1, int i2, int i3) {
+                    if (i1 < i3) {
+                        LinearLayoutManager linearLayoutManager = (LinearLayoutManager) msgRecyclerView.getLayoutManager();
+
+                        if (!isOldMessagesLoading) {
+                            if (linearLayoutManager != null && linearLayoutManager.findFirstCompletelyVisibleItemPosition() == 0) {
+                                loadPreviousMessages();
+                                isOldMessagesLoading = true;
+                            }
+                        }
+                    }
+                }
+            });
+        } else {
+            msgRecyclerView.setOnScrollListener(new RecyclerView.OnScrollListener() {
+                @Override
+                public void onScrollStateChanged(@NonNull @NotNull RecyclerView recyclerView, int newState) {
+                    super.onScrollStateChanged(recyclerView, newState);
+                }
+
+                @Override
+                public void onScrolled(@NonNull @NotNull RecyclerView recyclerView, int dx, int dy) {
+                    super.onScrolled(recyclerView, dx, dy);
+                    if (dy < 0) {
+                        LinearLayoutManager linearLayoutManager = (LinearLayoutManager) msgRecyclerView.getLayoutManager();
+
+                        if (!isOldMessagesLoading) {
+                            if (linearLayoutManager != null && linearLayoutManager.findFirstCompletelyVisibleItemPosition() == 0) {
+                                loadPreviousMessages();
+                                isOldMessagesLoading = true;
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    private void loadPreviousMessages() {
+        if (messages.get(0).getMessageIndex() > 0){
+            conversation.getMessagesBefore(messages.get(0).getMessageIndex()-1, LOAD_MESSAGE_COUNT, new CallbackListener<List<Message>>() {
+                @Override
+                public void onSuccess(List<Message> result) {
+                    isOldMessagesLoading = false;
+                    topProgress.setVisibility(View.GONE);
+                    result.addAll(messages);
+                    messages.clear();
+                    messages.addAll(result);
+                    conversationsMessageRVAdapter.notifyDataSetChanged();
+                }
+            });
+        }
+    }
+
+    @Override
+    public void onMessageAdded(final Message message) {
+        Log.d(TAG, "Message added");
+        messages.add(message);
+        conversationsMessageRVAdapter.notifyDataSetChanged();
+        msgRecyclerView.smoothScrollToPosition(messages.size()-1);
+    }
+
+    @Override
+    public void onMessageUpdated(Message message, Message.UpdateReason updateReason) {
+        Log.d(TAG, "Message updated: " + message.getMessageBody());
+    }
+
+    @Override
+    public void onMessageDeleted(Message message) {
+        Log.d(TAG, "Message deleted");
+    }
+
+    @Override
+    public void onParticipantAdded(Participant participant) {
+        Log.d(TAG, "Participant added: " + participant.getIdentity());
+    }
+
+    @Override
+    public void onParticipantUpdated(Participant participant, Participant.UpdateReason updateReason) {
+        Log.d(TAG, "Participant updated: " + participant.getIdentity() + " " + updateReason.toString());
+    }
+
+    @Override
+    public void onParticipantDeleted(Participant participant) {
+        Log.d(TAG, "Participant deleted: " + participant.getIdentity());
+    }
+
+    @Override
+    public void onTypingStarted(Conversation conversation, Participant participant) {
+        Log.d(TAG, "Started Typing: " + participant.getIdentity());
+    }
+
+    @Override
+    public void onTypingEnded(Conversation conversation, Participant participant) {
+        Log.d(TAG, "Ended Typing: " + participant.getIdentity());
+    }
+
+    @Override
+    public void onSynchronizationChanged(Conversation conversation) {
+
     }
 }
--- app/src/main/java/com/vaibhav/letschat/fcm/FCMListenerService.java
+++ app/src/main/java/com/vaibhav/letschat/fcm/FCMListenerService.java
@@ -1,9 +1,11 @@
 package com.vaibhav.letschat.fcm;
 
+import android.app.ActivityManager;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
@@ -28,6 +30,8 @@ import com.vaibhav.letschat.R;
 import org.jetbrains.annotations.NotNull;
 import org.json.JSONObject;
 
+import java.util.List;
+
 public class FCMListenerService extends FirebaseMessagingService {
 
     NotificationManager notificationManager;
@@ -51,70 +55,88 @@ public class FCMListenerService extends FirebaseMessagingService {
             Log.d("FCM", "Notification Message Body: " + remoteMessage.getNotification().getBody());
             Log.e("FCM", "We do not parse notification body - leave it to system");
         }
+        if (isAppInBackground(getApplicationContext())) {
+            notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
-        notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+            if (remoteMessage.getData().size() > 0) {
+                JSONObject obj = new JSONObject(remoteMessage.getData());
+                Log.d("FCM", "onMessageReceived: " + obj);
+                JsonElement mJson = JsonParser.parseString(obj.toString());
+                Gson gson = new Gson();
+                NewMessageNotificationModel notificationModel = gson.fromJson(mJson, NewMessageNotificationModel.class);
 
-        if (remoteMessage.getData().size()>0){
-            JSONObject obj = new JSONObject(remoteMessage.getData());
-            Log.d("FCM", "onMessageReceived: "+obj);
-            JsonElement mJson =  JsonParser.parseString(obj.toString());
-            Gson gson = new Gson();
-            NewMessageNotificationModel notificationModel = gson.fromJson(mJson, NewMessageNotificationModel.class);
+                String body = notificationModel.getTwiBody();
+                String msg = body.substring(getStartIndexOfMessage(body) + 1);
 
-            String body = notificationModel.getTwiBody();
-            String msg = body.substring(getStartIndexOfMessage(body)+1);
-
-            Intent intent = new Intent(this, ConversationsActivity.class);
-            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
-            Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+                Intent intent = new Intent(this, ConversationsActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+                Uri uri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
 
 
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                AudioAttributes attributes = new AudioAttributes.Builder()
-                        .setUsage(AudioAttributes.USAGE_NOTIFICATION)
-                        .build();
-                notificationChannel = new NotificationChannel(CHANNEL_ID, MSG_NOTIFICATION_DESCRIPTION, NotificationManager.IMPORTANCE_HIGH);
-                notificationChannel.enableLights(true);
-                notificationManager.createNotificationChannel(notificationChannel);
-                notificationChannel.setSound(uri, attributes);
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    AudioAttributes attributes = new AudioAttributes.Builder()
+                            .setUsage(AudioAttributes.USAGE_NOTIFICATION)
+                            .build();
+                    notificationChannel = new NotificationChannel(CHANNEL_ID, MSG_NOTIFICATION_DESCRIPTION, NotificationManager.IMPORTANCE_HIGH);
+                    notificationChannel.enableLights(true);
+                    notificationManager.createNotificationChannel(notificationChannel);
+                    notificationChannel.setSound(uri, attributes);
 
-                notificationManager.createNotificationChannel(notificationChannel);
+                    notificationManager.createNotificationChannel(notificationChannel);
 
-                builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                        .setSmallIcon(R.drawable.ic_baseline_message_24)
-                        .setContentTitle(notificationModel.getAuthor())
-                        .setContentText(msg)
-                        .setAutoCancel(true)
-                        .setSound(uri)
-                        .setDefaults(NotificationCompat.DEFAULT_ALL)
-                        .setContentIntent(pendingIntent);
-            } else {
-                builder = new  NotificationCompat.Builder(this, CHANNEL_ID)
-                        .setSmallIcon(R.drawable.ic_baseline_message_24)
-                        .setContentTitle(notificationModel.getAuthor())
-                        .setContentText(msg)
-                        //.setDefaults(NotificationCompat.DEFAULT_ALL)
-                        .setSound(uri)
-                        .setAutoCancel(true)
-                        .setPriority(NotificationCompat.PRIORITY_MAX)
-                        .setContentIntent(pendingIntent);
+                    builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                            .setSmallIcon(R.drawable.ic_baseline_message_24)
+                            .setContentTitle(notificationModel.getAuthor())
+                            .setContentText(msg)
+                            .setAutoCancel(true)
+                            .setSound(uri)
+                            .setDefaults(NotificationCompat.DEFAULT_ALL)
+                            .setContentIntent(pendingIntent);
+                } else {
+                    builder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                            .setSmallIcon(R.drawable.ic_baseline_message_24)
+                            .setContentTitle(notificationModel.getAuthor())
+                            .setContentText(msg)
+                            //.setDefaults(NotificationCompat.DEFAULT_ALL)
+                            .setSound(uri)
+                            .setAutoCancel(true)
+                            .setPriority(NotificationCompat.PRIORITY_MAX)
+                            .setContentIntent(pendingIntent);
+                }
+                notificationManager.notify(notificationID, builder.build());
+
             }
-            notificationManager.notify(notificationID, builder.build());
-
         }
 
     }
 
-    private int getStartIndexOfMessage(String msg){
+    private int getStartIndexOfMessage(String msg) {
         int colonCount = 0, index = 0;
-        while (colonCount<2){
-            if (msg.charAt(index) == ':'){
+        while (colonCount < 2) {
+            if (msg.charAt(index) == ':') {
                 colonCount++;
             }
             index++;
         }
         return index;
+    }
+
+    private boolean isAppInBackground(Context context) {
+        boolean isInBackground = true;
+
+        ActivityManager am = (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        List<ActivityManager.RunningAppProcessInfo> runningProcesses = am.getRunningAppProcesses();
+        for (ActivityManager.RunningAppProcessInfo processInfo : runningProcesses) {
+            if (processInfo.importance == ActivityManager.RunningAppProcessInfo.IMPORTANCE_FOREGROUND) {
+                for (String activeProcess : processInfo.pkgList) {
+                    if (activeProcess.equals(context.getPackageName())) {
+                        isInBackground = false;
+                    }
+                }
+            }
+        }
+        return isInBackground;
     }
 
     @Override
